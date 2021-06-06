85c675c6b92032af69973d2a729c039a4a2db8d8
Zendard <72878733+Zendard@users.noreply.github.com>
2021-06-06 12:58:44 +0200

    Add files via upload

---
+++ install programs.bat
@@ -0,0 +1,16 @@
+winget install "Google Chrome"
+winget install "Discord"
+winget install "WinRAR"
+winget install "Notepad++"
+winget install "Epic Games Launcher"
+winget install "Steam"
+winget install "Free Download Manager"
+winget install --id Oracle.JavaRuntimeEnvironment
+winget install "OBS Studio"
+print ""
+print ""
+print ""
+print "All programs succesfully installed"
+print "Your pc will restart in 15 seconds to complete the installation"
+shutdown.exe /r /t 15
+pause
